f4b05194dcaff1c01eab72ef7801e65044572aa8
Mohammed Tanzeem <mohammedtanzeem15@gmail.com>
2025-09-02 12:22:02 +0530

    workflow added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// For GitHub Pages, uncomment base and set to '/<repo-name>/'
 export default defineConfig({
   plugins: [react()],
-  // base: '/ShopSphere/',
+  base: '/ShopSphere/',
 })
